7fce2c89227f6ede0713a51efeeab0efe4fbc251
dev-mauricioAB <dev.mauricioalezandre@gmail.com>
2020-07-25 19:14:59 -0300

    Parametro alterado na funcao findByEmail()

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -18,7 +18,9 @@ class CreateCustomerService {
   ) { }
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    const checkCustomerExists = await this.customersRepository.findByEmail()
+    const checkCustomerExists = await this.customersRepository.findByEmail(
+      email,
+    )
 
     if (checkCustomerExists) {
       throw new AppError('Email address already used.')
